28ab8d72120d695a0f5ee6d3d076abf39d86cf81
ThomasLaneyrie <thomas.laneyrie@gmail.com>
2023-01-19 14:42:19 +0100

    Exercice journalistes

--- 00_journalists.rb
+++ 00_journalists.rb
@@ -1,23 +1,9 @@
-# Test pour les choix, restera plus qu'à mettre dans chaque case la méthode qui va bien.
-# arr_choix_question = Array.new()
-# arr_choix_question = ["Combien il y a de Handle dans cette array ? Tapez 0", "Quel est le handle le plus court de cette liste ? Tapez 1"]
-# puts arr_choix_question
-
-# puts "\nQuelle numéro souhaitez-vous suivre ? \nEntre 0 et 1"
-# choix = gets.chomp.to_i
-
-# case choix
-# when 0
-# nombre_handle
-# when 1 
-# plus_court_handle
-# end
-#                             -----------------------------------------------
-
+def length(arr)
+  puts "Nombre de handle dans cette fucking list : #{arr.length}"
+end
 
 def shorter(arr)    #A améliorer car me sort la 1ere valeur de l'array avec le nom le plus court, mais pas plusieurs
-  print "Voici le array : #{arr}\n"
-  puts "Voici le handle plus court : #{arr.min {|a, b| a.length <=> b.length}}\n"
+  puts "Voici le handle le plus court de la liste : #{arr.min {|a, b| a.length <=> b.length}}\n"
 end
 
 def handle_X_carac(arr, number)
@@ -43,36 +29,55 @@ def handle_uppercase(arr)
 end
 
 def alphabetic_sort (arr)
-  puts arr.sort_by!(&:downcase)
+  print "Voici la liste par ordre alphabétique :\n #{arr.sort_by!(&:downcase)}"
 end
 
 def length_sort(arr)
   # puts arr.sort_by!(&:length) FONCTIONNE EGALEMENT
-  puts arr.sort!{|x, y| x.length <=> y.length} 
+  print "Voici la liste triée par taille de handle :\n #{arr.sort!{|x, y| x.length <=> y.length}}"
 end
 
 def find_someone(arr,name)
-  puts arr.find_index(name)
+  puts "Voici la position de #{name} dans la liste : #{arr.find_index(name)}"
 end
 
 def sort_size_handle(arr)
-  # max = (arr.max {|a, b| a.length <=> b.length}).length
-  # puts max
   for number in 1..(arr.max {|a, b| a.length <=> b.length}).length
     puts "Nombre de handle avec #{number} caractères : #{arr.count {|i| i.length == number}}"
   end
 end
 
-#Début code normal
-arr = Array.new()
-arr = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau", "@Yopla", "@Salop", "@cocos", "@salut"]
-number = 5
-name="@Salop"
 
-# shorter(arr)
-# handle_X_carac(arr,number)
-# handle_uppercase(arr)
-# alphabetic_sort (arr)
-# length_sort(arr)
-# find_someone(arr,name)
-sort_size_handle(arr)
+
+# Début code normal
+arr = Array.new()
+arr =  ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+number = 5
+name="@epenser"
+
+# Test pour les choix, restera plus qu'à mettre dans chaque case la méthode qui va bien.
+arr_choix_question = ["- Combien il y a de Handle dans cette array ? Tapez 0", "- Quel est le handle le plus court de cette liste ? Tapez 1", "- Combien y-a t-il de handle contenant 5 caractères ? Tapez 2", "- Combien de handle commencent par une majuscule ? Tapez 3", "- Voulez-vous que je trie la liste des handle par ordre alphabétique ? Tapez 4", "- Voulez-vous que je trie la liste des handle par taille de handle ? Tapez 5", "- Voulez-vous connaitre la position de la personne @epenser dans la liste ? Tapez 6","- Voulez-voir voir une répartition des handle en fonction de leur taille ? Tapez 7"]
+puts "\nA l'ancienne, tapez votre choix en fonction de ce que vous voulez (On se croirait aux votes de la star Academy hein) : \n"
+puts arr_choix_question
+puts "\nQuelle numéro souhaitez-vous suivre ? \nEntre 0 et 7"
+print "> "
+choix = gets.chomp.to_i
+
+case choix
+  when 0
+    length(arr)
+  when 1 
+    shorter(arr)
+  when 2 
+    handle_X_carac(arr,number)
+  when 3
+    handle_uppercase(arr)
+  when 4
+    alphabetic_sort (arr)
+  when 5
+    length_sort(arr)
+  when 6
+    find_someone(arr,name)
+  when 7
+    sort_size_handle(arr)
+  end
